82045950a37b9bde28f71739aeb4c5cdba77eaf0
AZEEMAZAM1 <MUHAMMADAZEEM3010@GMAIL.COM>
2025-04-23 02:41:27 +0100

    Update: added new features

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,13 @@
+// filepath: /Users/user/Desktop/MyApp/frontend/src/screens/HomeScreen.js
+
+import React from 'react';
+
+const HomeScreen = () => {
+  return (
+    <div>
+      <h1>Welcome to the Delivery Driver App</h1>
+    </div>
+  );
+};
+
+export default HomeScreen;
---
+++ src/styles/globalStyles.js
@@ -0,0 +1,14 @@
+// filepath: /Users/user/Desktop/MyApp/frontend/src/styles/globalStyles.js
+
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+    background-color: #f4f4f4;
+  }
+`;
+
+export default GlobalStyles;
